d6bb4ff6807819f864e46686a4f09874b8c6c0a7
eduardo <eduardobnhr@gmail.com>
2023-07-27 12:39:27 -0400

    add styles

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,4 +1,6 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import './App.css';
+
 
 function App() {
   const [todos, setTodos] = useState([
@@ -25,6 +27,11 @@ function App() {
           <div className="todo">
             <div className="content">
               <p>{todo.text}</p>
+              <p className="category">{todo.category}</p> 
+            </div>
+            <div>
+              <button>Completar</button>
+              <button>X</button>
             </div>
           </div>
         ))}
