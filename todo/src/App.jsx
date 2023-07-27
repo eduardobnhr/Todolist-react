bf4acf9eda0a33dc41c4a720f6424a7d32f9dbad
eduardo <eduardobnhr@gmail.com>
2023-07-27 13:05:36 -0400

    add component Todoform

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
+import Todo from './components/Todo';
 import './App.css';
+import TodoForm from './components/TodoForm';
 
 
 function App() {
@@ -24,18 +26,10 @@ function App() {
       <h1>Lista de tarefas</h1>
       <div className="todo-list">
         {todos.map((todo) => (
-          <div className="todo">
-            <div className="content">
-              <p>{todo.text}</p>
-              <p className="category">{todo.category}</p> 
-            </div>
-            <div>
-              <button>Completar</button>
-              <button>X</button>
-            </div>
-          </div>
+          <Todo todo ={todo} /> 
         ))}
       </div>
+      <TodoForm />
     </div>
   );
   
